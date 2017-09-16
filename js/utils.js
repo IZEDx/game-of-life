6d91fd4e6609c905dee8e6b23684b933f4384036
Niklas Kühtmann <admin@i-zed.net>
2017-09-16 16:13:53 +0200

    Forgot utils.js

---
+++ js/utils.js
@@ -0,0 +1,7 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function nop() { }
+    exports.nop = nop;
+});
+//# sourceMappingURL=utils.js.map
